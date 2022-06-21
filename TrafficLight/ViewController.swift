321fcfc1c00b5db9bffbc69853b3ccf25916ac33
Amuzey <ivanovad97@gmail.com>
2022-06-21 12:03:34 +0500

    work with logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -14,13 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var startButton: UIButton!
     
+    private var lightTurnOff: CGFloat = 0.3
+    private var lightTurnOn: CGFloat = 1
+    private var tapCount = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startButton.layer.cornerRadius = 15
-        redView.alpha = 0.3
-        yellowView.alpha = 0.3
-        greenView.alpha = 0.3
+        redView.alpha = lightTurnOff
+        yellowView.alpha = lightTurnOff
+        greenView.alpha = lightTurnOff
         
     }
     
@@ -30,22 +34,20 @@ class ViewController: UIViewController {
         yellowView.layer.cornerRadius = yellowView.frame.width / 2
         greenView.layer.cornerRadius = greenView.frame.width / 2
     }
-    
-   private var tapCount = 1
-    
+
     @IBAction func startButtonTap() {
         
         startButton.setTitle("NEXT", for: .normal)
         
         if tapCount % 3 == 1 {
-            greenView.alpha = 0.3
-            redView.alpha = 1
+            greenView.alpha = lightTurnOff
+            redView.alpha = lightTurnOn
         } else if tapCount % 3 == 2 {
-            redView.alpha = 0.3
-            yellowView.alpha = 1
+            redView.alpha = lightTurnOff
+            yellowView.alpha = lightTurnOn
         } else if tapCount % 3 == 0 {
-            yellowView.alpha = 0.3
-            greenView.alpha = 1
+            yellowView.alpha = lightTurnOff
+            greenView.alpha = lightTurnOn
         }
         tapCount += 1
     }
